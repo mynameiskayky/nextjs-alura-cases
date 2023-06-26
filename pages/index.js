93d8b5a12eb905416c65a87468cb7aaf74728436
nynameiskayky <kaykyvieiraa@hotmail.com>
2023-06-26 20:30:48 -0300

    aula 3: estilizando o projeto

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import Link from "../src/components/Link"
 
 export default function HomePage() {
+  
+  function Title({ children, as }) {
+    const Tag = as
+    return (
+      <>
+        <Tag>
+          {children}
+        </Tag>
+        <style jsx>{`
+          ${Tag} {
+            color: red;
+          }
+        `}</style>
+      </>
+    )
+  }
+
   return (
     <div>
-      <h1>Alura Cases - Home asd</h1>
+      <Title as="h1">Alura Cases - Home</Title>
       <Link href="/faq">
         Ir para o FAQ
       </Link>
