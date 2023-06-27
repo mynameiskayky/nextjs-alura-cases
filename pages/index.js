29b4e8729557f7bce9fe1cc04b6b5b051f4c4ac1
nynameiskayky <kaykyvieiraa@hotmail.com>
2023-06-27 13:56:00 -0300

    aula 3: componentes e tema do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,35 @@
-import Link from "../src/components/Link"
+import HomeScreen from "../src/screens/HomeScreen";
 
-export default function HomePage() {
+export default HomeScreen
+
+// import Link from "../src/components/Link"
+// import Footer from "../src/components/patterns/Footer"
+
+// export default function HomePage() {
   
-  function Title({ children, as }) {
-    const Tag = as
-    return (
-      <>
-        <Tag>
-          {children}
-        </Tag>
-        <style jsx>{`
-          ${Tag} {
-            color: red;
-          }
-        `}</style>
-      </>
-    )
-  }
+//   function Title({ children, as }) {
+//     const Tag = as
+//     return (
+//       <>
+//         <Tag>
+//           {children}
+//         </Tag>
+//         <style jsx>{`
+//           ${Tag} {
+//             color: red;
+//           }
+//         `}</style>
+//       </>
+//     )
+//   }
 
-  return (
-    <div>
-      <Title as="h1">Alura Cases - Home</Title>
-      <Link href="/faq">
-        Ir para o FAQ
-      </Link>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <Title as="h1">Alura Cases - Home</Title>
+//       <Link href="/faq">
+//         Ir para o FAQ
+//       </Link>
+//       <Footer />
+//     </div>
+//   )
+// }
