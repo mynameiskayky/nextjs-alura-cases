b9d7b7fb2571ed732d562cbae5f5ad7308d6c911
nynameiskayky <kaykyvieiraa@hotmail.com>
2023-07-23 18:17:41 -0300

    the end

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react"
+import Head from "next/head"
 import Link from "../src/components/Link"
+import PageTitle from "../src/components/PageTitle"
 
 export async function getStaticProps() {
   const faqUrlAPI = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
@@ -17,6 +18,7 @@ export async function getStaticProps() {
 export default function FAQPage({ faqData }) {
   return (
     <div>
+      <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
       <h1>Alura Cases - FAQ</h1>
       <Link href="/">
         Voltar para a Home
